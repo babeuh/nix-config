4be8ddf47942fc38c9ed5cc6648130f893864494
Babeuh <60193302+babeuh@users.noreply.github.com>
2023-05-21 20:58:42 +0200

    rm(hosts): remove shell-switch todo

--- hosts/common/shell.nix
+++ hosts/common/shell.nix
@@ -1,5 +1,4 @@
 { pkgs, ... }: {
-  # TODO: Consider moving to nushell or other rust shells, check that github issue IDK
   programs.fish.enable = true;
   users.defaultUserShell = pkgs.fish;
   environment.shells = with pkgs; [ fish ];
