0d8404f6ff0069dcaaab542f22998e9350562aeb
Babeuh <60193302+babeuh@users.noreply.github.com>
2023-07-12 23:23:03 +0200

    change(multiviewer-for-f1): simplify

--- pkgs/multiviewer-for-f1.nix
+++ pkgs/multiviewer-for-f1.nix
@@ -2,6 +2,7 @@
 , fetchurl
 , lib
 , makeWrapper
+, autoPatchelfHook
 , dpkg
 , alsa-lib
 , at-spi2-atk
@@ -13,7 +14,6 @@
 , glib
 , gtk3
 , libdrm
-, libpulseaudio
 , libudev0-shim
 , libxkbcommon
 , mesa
@@ -21,29 +21,28 @@
 , nss
 , pango
 , xorg
-, wayland
 }:
+
 let
-  inherit (stdenv.hostPlatform) system;
-  pname = "multiviewer-for-f1";
   version = "1.24.0";
   id = "116221064";
 
-  meta = with lib; {
-    description = "Unofficial desktop client for F1 TV®";
-    homepage = "https://multiviewer.app";
-    downloadPage = "https://multiviewer.app/download";
-    license = licenses.unfree;
-    maintainers = with maintainers; [ babeuh ];
-    platforms = [ "x86_64-linux" ];
-  };
+in
+stdenv.mkDerivation {
+  pname = "multiviewer-for-f1";
+  inherit version;
 
   src = fetchurl {
     url = "https://releases.multiviewer.dev/download/${id}/multiviewer-for-f1_${version}_amd64.deb";
     sha256 = "sha256-eCtCw0b8XnQ7EUBZAw6tloY5EIUnHNB3GuWQV61jAIo=";
   };
 
-  deps = [
+  nativeBuildInputs = [
+    makeWrapper
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
     alsa-lib
     at-spi2-atk
     cairo
@@ -54,8 +53,6 @@ let
     glib
     gtk3
     libdrm
-    libpulseaudio
-    libudev0-shim
     libxkbcommon
     mesa
     nspr
@@ -68,40 +65,40 @@ let
     xorg.libXext
     xorg.libXfixes
     xorg.libXrandr
-    wayland
   ];
-in
-stdenv.mkDerivation {
-    inherit pname version src meta;
 
-    nativeBuildInputs = [ makeWrapper ];
+  dontBuild = true;
+  dontConfigure = true;
 
-    dontBuild = true;
-    dontConfigure = true;
+  unpackPhase = ''
+    runHook preUnpack
+    ${dpkg}/bin/dpkg-deb -x $src . || true
+    runHook postUnpack
+  '';
 
-    unpackPhase = ''
-      ${dpkg}/bin/dpkg-deb -x $src . || true
-    '';
+  installPhase = ''
+    runHook preInstall
 
-    installPhase = ''
-      runHook preInstall
+    mkdir -p $out/bin $out/share/multiviewer-for-f1
 
-      mkdir -p $out/bin $out/share/multiviewer-for-f1
+    cp -a usr/share/* $out/share
+    cp -a usr/lib/multiviewer-for-f1 $out/share/
+    mv $out/share/multiviewer-for-f1/"MultiViewer for F1" $out/share/multiviewer-for-f1/multiviewer-for-f1
 
-      cp -a usr/share/* $out/share
-      cp -a usr/lib/multiviewer-for-f1 $out/share/
-      mv $out/share/multiviewer-for-f1/"MultiViewer for F1" $out/share/multiviewer-for-f1/multiviewer-for-f1
+    makeWrapper $out/share/multiviewer-for-f1/multiviewer-for-f1 $out/bin/multiviewer-for-f1 \
+      --add-flags $out/share/multiviewer-for-f1/resources/app \
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations}}" \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ libudev0-shim ] }:$out/share/multiviewer-for-f1"
 
-      rpath="$out/share/multiviewer-for-f1"
-      patchelf \
-        --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-        --set-rpath $rpath $out/share/multiviewer-for-f1/multiviewer-for-f1
+    runHook postInstall
+  '';
 
-      makeWrapper $out/share/multiviewer-for-f1/multiviewer-for-f1 $out/bin/multiviewer-for-f1 \
-        --add-flags $out/share/multiviewer-for-f1/resources/app \
-        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations}}" \
-        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath deps }:$out/share/multiviewer-for-f1"
-
-      runHook postInstall
-    '';
-  }
+  meta = with lib; {
+    description = "Unofficial desktop client for F1 TV®";
+    homepage = "https://multiviewer.app";
+    downloadPage = "https://multiviewer.app/download";
+    license = licenses.unfree;
+    maintainers = with maintainers; [ babeuh ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
