88813062772f5139d128d9d24516dd66c6ea6fb4
Babeuh <babeuh@rlglr.fr>
2023-07-29 16:02:18 +0200

    fix(home/desktop): playerctl daemon

--- home/common/desktop/common/playerctl.nix
+++ home/common/desktop/common/playerctl.nix
@@ -1,3 +1,17 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ playerctl ];
+  home.packages = [ pkgs.playerctl ];
+  systemd.user.services.playerctld = {
+    Unit = {
+      Description = "MPRIS media player daemon";
+      After = "multi-user.target graphical.target";
+    };
+
+    Install.WantedBy = [ "default.target" ];
+
+    Service = {
+      ExecStart = "${pkgs.playerctl}/bin/playerctld";
+      Type = "dbus";
+      BusName = "org.mpris.MediaPlayer2.playerctld";
+    };
+  };
 }
