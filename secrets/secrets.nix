8133f0e0e1aee1d13dcc838a4c40a02698957676
Babeuh <babeuh@rlglr.fr>
2023-08-28 01:49:26 +0200

    change(secrets/secrets.nix): rename "yubikeys" to "keys"

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,9 @@
 let
-  yubikeys = {
+  keys = {
     main = "age1yubikey1qtgrhjey60y8zv62tek00fz3jvs2g3rd46zaphpd446an97aq9dycyta4y2";
     backup = "age1yubikey1qtufjgm5g7lem0na7sd4uhusk7z3uu2jpmknrfl62rpm6e957hr42kcamhw";
   };
 in
 {
-  "babeuh-password.age".publicKeys = [ yubikeys.main yubikeys.backup ];
+  "babeuh-password.age".publicKeys = [ keys.main keys.backup ];
 }
